1e835295939f2738144a1c15a4ab92819ffd5f8e
Matteo Meciani <mmeciani@ethz.ch>
2026-08-26 16:41:22 +0200

    Changed num cpus to 1 and added num threads = 1

--- scripts/submit_run.sh
+++ scripts/submit_run.sh
@@ -4,13 +4,16 @@
 #SBATCH --partition=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --time=00:10:00
 #SBATCH --output=logs/%j_cnn_run.out
 #SBATCH --error=logs/%j_cnn_run.err
 
 cd $SLURM_SUBMIT_DIR
 mkdir -p logs
+
+# Explicitly set number of threads to 1
+export OMP_NUM_THREADS=1
 
 echo "Job ID:  $SLURM_JOB_ID"
 echo "Node:    $SLURMD_NODENAME"
